1b7d20e0343eeb08d2d57e54f87b8b2bbba5bbd2
luungoc2005 <luungoc2005@gmail.com>
2019-10-17 05:58:51 +0800

    fix fp16 compat

--- collections/nemo_nlp/nemo_nlp/huggingface/albert.py
+++ collections/nemo_nlp/nemo_nlp/huggingface/albert.py
@@ -165,9 +165,9 @@ class BertEmbeddings(nn.Module):
         if token_type_ids is None:
             token_type_ids = torch.zeros_like(input_ids)
 
-        words_embeddings = self.embedding_proj(self.word_embeddings(input_ids))
-        position_embeddings = self.position_embeddings(position_ids)
-        token_type_embeddings = self.token_type_embeddings(token_type_ids)
+        words_embeddings = self.embedding_proj(self.word_embeddings(input_ids.long())).to(dtype=next(self.parameters()).dtype)
+        position_embeddings = self.position_embeddings(position_ids.long()).to(dtype=next(self.parameters()).dtype)
+        token_type_embeddings = self.token_type_embeddings(token_type_ids.long()).to(dtype=next(self.parameters()).dtype)
 
         embeddings = words_embeddings + position_embeddings + token_type_embeddings
         embeddings = self.LayerNorm(embeddings)
